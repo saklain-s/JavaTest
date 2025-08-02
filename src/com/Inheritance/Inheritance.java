cf4e38554a28af690caf1f325c082bb3b57334b7
saklain-s <123095572+saklain-s@users.noreply.github.com>
2025-08-02 12:35:01 +0530

    trees graphs and many more

--- src/com/Inheritance/Inheritance.java
+++ src/com/Inheritance/Inheritance.java
@@ -1,25 +1,25 @@
-package com.Inheritance;
-
-class Parent {
-    public Parent() {
-        System.out.println("Parent class");
-    }
-}
-
-class Child extends Parent {
-    public Child() {
-        System.out.println("Child");
-    }
-}
-
-class GrandChild extends Child {
-    public GrandChild() {
-        System.out.println("GrandChild");
-    }
-}
-
-public class Inheritance {
-    public static void main(String[] args) {
-        GrandChild p = new GrandChild();
-    }
-}
+//package com.Inheritance;
+//
+//class Parent {
+//    public Parent() {
+//        System.out.println("Parent class");
+//    }
+//}
+//
+//class Child extends Parent {
+//    public Child() {
+//        System.out.println("Child");
+//    }
+//}
+//
+//class GrandChild extends Child {
+//    public GrandChild() {
+//        System.out.println("GrandChild");
+//    }
+//}
+//
+//public class Inheritance {
+//    public static void main(String[] args) {
+//        GrandChild p = new GrandChild();
+//    }
+//}
